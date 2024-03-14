e6d3638a54b559067fb8fb20f5a7a8fdb2b73cab
krisztina-feketemolnar <163307409+krisztina-feketemolnar@users.noreply.github.com>
2024-03-14 09:21:52 +0100

    de

--- example_schema.sql
+++ example_schema.sql
@@ -1,7 +1,16 @@
-drop table if exists examples;
+drop table if people examples;
 
-create table `examples` (
+create table `people` (
   `id` int not null auto_increment,
-  `name` varchar(80) default null,
+  `given_name` varchar(80) default null,
+  `family_name` varchar(80) default null,
+  `date_of_birth` date default null,
+  `place_of_birth` int default null, --there can be places with same name but in different countriey, so I use id 
   primary key (`id`)
 );
+
+--people
+--given_name,family_name,date_of_birth,place_of_birth
+
+--places
+--city,county,country
